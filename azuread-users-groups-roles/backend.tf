ad3bffe30a30a0ad8dd4b295426d104bda419cba
Jagadish Vemugunta <jvemugunta@gmail.com>
2023-06-11 17:02:33 -0400

    upadted users

--- azuread-users-groups-roles/backend.tf
+++ azuread-users-groups-roles/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "centralus-jennasrunbookstf"
-    storage_account_name = "jennasrunbookstfstate"
-    container_name       = "jennasrunbookstf"
-    key                  = "azuread-jennasrunbookstf.tfstate"
+    resource_group_name  = "eastus-jagadishtf"
+    storage_account_name = "jagadishrunbookstfstate"
+    container_name       = "jagadishrunbookstf"
+    key                  = "eastus-jagadishtf.tfstate"
   }
 }
